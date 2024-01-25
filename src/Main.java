1137dc41a7db2ef9d4969353973873dc32c86ff5
Dmitriy Stroganov <Distrog@gmail.com>
2024-01-25 20:29:18 +0700

    Проверьте задачу из билета

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static String[] splitString(String s) {
         if (s.length() % 2 != 0) {
-            return new String[]{"Невозможно разделить строку ровно поровну"};
+            return new String[]{"Невозможно разделить строку поровну"};
         } else {
             return new String[]{s.substring(0, s.length() / 2), s.substring(s.length() / 2)};
         }
